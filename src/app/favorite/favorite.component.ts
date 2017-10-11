6dcd45a9467ba1d3fbb8f72b0c0005d822176f8b
Vibhor Agarwal <vibhor.agarwal@outlook.com>
2017-10-11 15:49:46 +0530

    5|62 - assignment/a4

--- src/app/favorite/favorite.component.ts
+++ src/app/favorite/favorite.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
     selector: 'favorite',
@@ -6,14 +6,30 @@ import { Component } from '@angular/core';
     styleUrls: ['./favorite.component.css']
 })
 export class FavoriteComponent {
-    private isSelected: boolean;
-    get IsSelected() {
-        return this.isSelected;
-    }
+
+    @Input('like-count') public likeCount: number;
+    @Input('is-selected') public isSelected: boolean;
+    @Output('change') change = new EventEmitter();
+
     constructor() {
     }
-    onClick($event){
+
+    onClick($event) {
+        this.likeCount += this.isSelected ? -1 : +1;
         this.isSelected = !this.isSelected;
-        console.log($event); 
-    } 
+        console.log($event);
+        this.change.emit(new FavoriteChangedEventArgs(!this.isSelected, this.isSelected, this));
+    }
+}
+
+export class FavoriteChangedEventArgs {
+    constructor(private oldValue: any, private newValue: any, private component?: FavoriteComponent) {
+    }
+
+    get OldValue() {
+        return this.oldValue;
+    }
+    get NewValue() {
+        return this.newValue;
+    }
 }
